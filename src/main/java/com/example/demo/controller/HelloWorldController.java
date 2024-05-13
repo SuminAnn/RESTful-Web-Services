f2e6ffe765c96bfca43e1493af67f3240fd944f2
SuminAnn <annsumin2066@naver.com>
2024-05-13 15:30:30 +0900

    PathVariable

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -1,10 +1,12 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.bean.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
-@RestController
+@RestController //@Controller + @ResponseBody
 public class HelloWorldController {
     // GET
     // URI : /hello-world
@@ -12,6 +14,15 @@ public class HelloWorldController {
     @GetMapping("/hello-world")
     public String helloworld() {
         return "Hello World";
+    }//문자열
+
+    @GetMapping("/hello-world-bean")
+    public HelloWorldBean helloworldBean() {
+        return new HelloWorldBean("Hello World!");
+    }//객체타입
+
+    @GetMapping("/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloworldBeanPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
-    
 }
